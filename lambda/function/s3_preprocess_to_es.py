38f3721278bca99771ddde026e0960e99e63b5ec
utanashati <utanashati@mail.ru>
2020-09-23 09:16:21 +0200

    Lambda Upd

--- lambda/function/s3_preprocess_to_es.py
+++ lambda/function/s3_preprocess_to_es.py
@@ -47,55 +47,71 @@ def handler(event, context):
         key = record['s3']['object']['key']
 
         # Get S3 object
-        try:
-            response = s3.select_object_content(
-                Bucket=bucket,
-                Key=key,
-                ExpressionType='SQL',
-                Expression="select * from s3object",
-                InputSerialization={
-                    'CompressionType': 'GZIP', 'JSON': {'Type': 'LINES'}},
-                OutputSerialization={'JSON': {}}
-            )
-        except ClientError as exc:
-            if exc.response['Error']['Code'] == 'NoSuchKey':
-                logger.error(
-                    '%s: %s Key: %s',
-                    exc.response['Error']['Code'],
-                    exc.response['Error']['Message'],
-                    key)
+        records = ''
+        repeat = True
+        while repeat:
+            try:
+                response = s3.select_object_content(
+                    Bucket=bucket,
+                    Key=key,
+                    ExpressionType='SQL',
+                    Expression="select * from s3object",
+                    InputSerialization={
+                        'CompressionType': 'GZIP', 'JSON': {'Type': 'LINES'}},
+                    OutputSerialization={'JSON': {}}
+                )
+            except ClientError as exc:
+                if exc.response['Error']['Code'] == 'NoSuchKey':
+                    logger.error(
+                        '%s: %s Key: %s',
+                        exc.response['Error']['Code'],
+                        exc.response['Error']['Message'],
+                        key)
+                    continue
+                else:
+                    raise exc
+
+            for event in response['Payload']:
+                # logger.error('Event:\n' + str(event))
+                if 'End' in event:
+                    repeat = False
+                if 'Records' in event:
+                    records += event['Records']['Payload'].decode('utf-8')
+
+        # bulk_data = []
+        recs = records.splitlines()
+        logger.debug('Num records: %s.', len(recs))
+        for i, rec in enumerate(recs):
+            try:
+                status = json.loads(rec)
+            except json.JSONDecodeError as exc:
+                logger.error('%s: %s', type(exc).__name__, str(exc))
+                logger.error('Rec:\n%s', str(rec))
                 continue
-            else:
-                raise exc
 
-        for event in response['Payload']:
-            if 'Records' in event:
-                records = event['Records']['Payload'].decode('utf-8')
+            index_name = \
+                ESEnv.INDEX_PREFIX + status['_tracking_info']['slug']
 
-                # bulk_data = []
-                for rec in records.splitlines():
-                    status = json.loads(rec)
-                    index_name = \
-                        ESEnv.INDEX_PREFIX + status['_tracking_info']['slug']
+            try:
+                es.create(
+                    index=index_name, id=status['id'],
+                    body=status, doc_type='tweet')
+                logger.debug('Loaded rec %d, id %d.', i, status['id'])
+            except ConflictError as exc:
+                # Happens when a document with the same ID
+                # already exists.
+                logger.debug('Rec %d, id %d already exists.', i, status['id'])
+                logger.error('%s: %s', type(exc).__name__, str(exc))
 
-                    try:
-                        es.create(
-                            index=index_name, id=status['id'],
-                            body=status, doc_type='tweet')
-                    except ConflictError as exc:
-                        # Happens when a document with the same ID
-                        # already exists.
-                        logger.error(exc)
+        # Does not raise ConflictError, just loads new vesrions
+        # of the same doc, if a duplicate.
+        #     bulk_doc = {
+        #         '_index': index_name,
+        #         '_type': 'tweet',
+        #         '_id': status['id'],
+        #         '_source': status
+        #     }
+        #     bulk_data.append(bulk_doc)
 
-                # Does not raise ConflictError, just loads new vesrions
-                # of the same doc, if a duplicate.
-                #     bulk_doc = {
-                #         '_index': index_name,
-                #         '_type': 'tweet',
-                #         '_id': status['id'],
-                #         '_source': status
-                #     }
-                #     bulk_data.append(bulk_doc)
-
-                # success, _ = bulk(es, bulk_data)
-                # logger.info('ElasticSearch indexed %d documents.', success)
+        # success, _ = bulk(es, bulk_data)
+        # logger.info('ElasticSearch indexed %d documents.', success)
