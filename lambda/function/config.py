bad5931b499b11c9ac672045ced636b76e8ffd51
utanashati <utanashati@mail.ru>
2020-11-23 14:28:24 +0100

    ConfigManager load fix

--- lambda/function/config.py
+++ lambda/function/config.py
@@ -1,23 +1,58 @@
 import logging
-import os
 import json
 
 from enum import Enum
 from typing import List, Dict, Set, Optional
 from dataclasses import dataclass, asdict, field
 
+from botocore.exceptions import ClientError
 import dacite
 
-from env import Env
+from env import AWSEnv
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
+def get_s3_object(s3_client, bucket, key, input_serialization):
+    # Get S3 object
+    records = b''
+    repeat = True
+    while repeat:
+        try:
+            response = s3_client.select_object_content(
+                Bucket=bucket,
+                Key=key,
+                ExpressionType='SQL',
+                Expression="select * from s3object",
+                InputSerialization=input_serialization,
+                OutputSerialization={'JSON': {}}
+            )
+        except ClientError as exc:
+            if exc.response['Error']['Code'] == 'NoSuchKey':
+                logger.error(
+                    '%s: %s Key: %s',
+                    exc.response['Error']['Code'],
+                    exc.response['Error']['Message'],
+                    key)
+                continue
+            else:
+                raise exc
+
+        for event in response['Payload']:
+            if 'End' in event:
+                repeat = False
+            if 'Records' in event:
+                records += event['Records']['Payload']
+
+    return records.decode('utf-8')
+
+
 class StorageMode(Enum):
     TEST_MODE = 1
     S3_ES = 2
-    S3_ES_NO_RETWEETS = 3
+    S3_ES_NO_UNMATCHED = 3
+    S3_ES_NO_RETWEETS = 4
 
 
 class ImageStorageMode(Enum):
@@ -50,10 +85,8 @@ class FilterConf:
 
 class ConfigManager():
     """Read, write and validate project configs."""
-    def __init__(self, config=None):
-        self.config_path = os.path.join(
-            Env.CONFIG_PATH, Env.STREAM_CONFIG_PATH)
-        self.config = self._load() if config is None else self._load(config)
+    def __init__(self, s3_client):
+        self.config = self._load(s3_client)
         self.filter_config = self._pool_config()
 
     def get_conf_by_slug(self, slug):
@@ -70,14 +103,14 @@ class ConfigManager():
                     for key in ['lang', 'keywords', 'slug']}
                 return info
 
-    def write(self):
-        with open(self.config_path, 'w') as f:
+    def write(self, path):
+        with open(path, 'w') as f:
             json.dump([asdict(conf) for conf in self.config], f, indent=4)
 
-    def _load(self, raw=None):
-        if raw is None:
-            with open(self.config_path, 'r') as f:
-                raw = json.load(f)
+    def _load(self, s3_client):
+        raw = get_s3_object(
+            s3_client, AWSEnv.BUCKET_NAME, AWSEnv.CONFIG_S3_KEY,
+            {'CompressionType': 'NONE', 'JSON': {'Type': 'DOCUMENT'}})
         config = []
         for conf in raw:
             config.append(dacite.from_dict(
--- stream/config.py
+++ stream/config.py
@@ -1,17 +1,58 @@
 import logging
-import os
 import json
 
 from enum import Enum
 from typing import List, Dict, Set, Optional
 from dataclasses import dataclass, asdict, field
 
+import boto3
+from botocore.exceptions import ClientError
 import dacite
 
-from .env import Env
+from env import AWSEnv
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+s3 = boto3.client(
+    's3',
+    region_name=AWSEnv.REGION,
+    aws_access_key_id=AWSEnv.ACCESS_KEY_ID,
+    aws_secret_access_key=AWSEnv.SECRET_ACCESS_KEY)
+
+
+def get_s3_object(s3_client, bucket, key, input_serialization):
+    # Get S3 object
+    records = b''
+    repeat = True
+    while repeat:
+        try:
+            response = s3_client.select_object_content(
+                Bucket=bucket,
+                Key=key,
+                ExpressionType='SQL',
+                Expression="select * from s3object",
+                InputSerialization=input_serialization,
+                OutputSerialization={'JSON': {}}
+            )
+        except ClientError as exc:
+            if exc.response['Error']['Code'] == 'NoSuchKey':
+                logger.error(
+                    '%s: %s Key: %s',
+                    exc.response['Error']['Code'],
+                    exc.response['Error']['Message'],
+                    key)
+                continue
+            else:
+                raise exc
+
+        for event in response['Payload']:
+            if 'End' in event:
+                repeat = False
+            if 'Records' in event:
+                records += event['Records']['Payload']
+
+    return records.decode('utf-8')
 
 
 class StorageMode(Enum):
@@ -51,10 +92,8 @@ class FilterConf:
 
 class ConfigManager():
     """Read, write and validate project configs."""
-    def __init__(self, config=None):
-        self.config_path = os.path.join(
-            Env.CONFIG_PATH, Env.STREAM_CONFIG_PATH)
-        self.config = self._load() if config is None else self._load(config)
+    def __init__(self, s3_client=s3):
+        self.config = self._load(s3_client)
         self.filter_config = self._pool_config()
 
     def get_conf_by_slug(self, slug):
@@ -71,14 +110,14 @@ class ConfigManager():
                     for key in ['lang', 'keywords', 'slug']}
                 return info
 
-    def write(self):
-        with open(self.config_path, 'w') as f:
+    def write(self, path):
+        with open(path, 'w') as f:
             json.dump([asdict(conf) for conf in self.config], f, indent=4)
 
-    def _load(self, raw=None):
-        if raw is None:
-            with open(self.config_path, 'r') as f:
-                raw = json.load(f)
+    def _load(self, s3_client):
+        raw = get_s3_object(
+            s3_client, AWSEnv.BUCKET_NAME, AWSEnv.CONFIG_S3_KEY,
+            {'CompressionType': 'NONE', 'JSON': {'Type': 'DOCUMENT'}})
         config = []
         for conf in raw:
             config.append(dacite.from_dict(
@@ -93,3 +132,6 @@ class ConfigManager():
             filter_conf.keywords.update(conf.keywords)
             filter_conf.lang.update(conf.lang)
         return filter_conf
+
+
+config_manager = ConfigManager()
