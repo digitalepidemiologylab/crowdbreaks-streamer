a51596da60560a2fc6eaffe710be027fd0511926
utanashati <utanashati@mail.ru>
2020-12-08 16:15:41 +0100

    Run lambda fix 2

--- lambda/run.py
+++ lambda/run.py
@@ -15,5 +15,5 @@ def main():
     logger.info(os.getcwd())
     zip_lambda_func()
     zip_lambda_layer()
-    create_lambda_layer(push_layer=True, create_layer=True)
-    create_s3_to_es_lambda(push_func=True)
+    create_lambda_layer(push_layer=False, create_layer=True)
+    create_s3_to_es_lambda(push_func=False)
