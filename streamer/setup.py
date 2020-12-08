626020858bdb6f55f08a46ef97ccff3fd2eb984c
utanashati <utanashati@mail.ru>
2020-12-08 15:02:11 +0100

    Trying to setup awstools

--- streamer/setup.py
+++ streamer/setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
         'python-dotenv', 'aenum', 'dacite', 'tweepy',
         'boto3', 'elasticsearch', 'requests_aws4auth',
         'twiprocess @ git+https://github.com/crowdbreaks/twiprocess.git',
-        'awstools @ https://github.com/crowdbreaks/streamer.git@master#subdirectory=awstools/'],
+        'awstools @ git+ssh://git@github.com/owner/repo-name.git@master#"egg=awstools&subdirectory=awstools"'],
     entry_points={'console_scripts': [
         'run-stream=stream.run:main']},
     classifiers=[
