504bdbcd2c24204346ff6291e8660e6e1bd8db8e
utanashati <utanashati@mail.ru>
2021-01-20 19:10:35 +0100

    Python logs in lambda action 4

--- lambda/run.py
+++ lambda/run.py
@@ -11,10 +11,13 @@ logger.setLevel(logging.INFO)
 
 
 def main():
-    print('blabla')
     logger.info(os.path.dirname(os.path.realpath(__file__)))
     logger.info(os.getcwd())
     zip_lambda_func()
     zip_lambda_layer()
     create_lambda_layer(push_layer=True, create_layer=False)
     create_s3_to_es_lambda(push_func=True)
+
+
+if __name__ == "__main__":
+    main()
