1345476bb809f7cb251a87a1ae74f60fd1ebc9d4
utanashati <utanashati@mail.ru>
2021-06-07 22:21:43 +0200

    Modularity upd

--- lambda-streamer-management/function/lambda.py
+++ lambda-streamer-management/function/lambda.py
@@ -21,8 +21,8 @@ def check_if_currently_active(cluster_name, service_name):
     for service in response['services']:
         if service['serviceName'] == service_name:
             name_in_response = True
-            service_count = service[f'runningCount']
-            service_count += service[f'pendingCount']
+            service_count = service['runningCount']
+            service_count += service['pendingCount']
             if service_count == 0:
                 active = False
     if name_in_response is False:
@@ -71,9 +71,32 @@ def wait_for_desired_count(
         service_name, cluster_name, service_count)
 
 
+def start_streamer():
+    # To start streaming, set the desired count to 1 and wait
+    # until a task is running
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ecs.html#ECS.Client.update_service
+    ecs.update_service(
+        cluster=ECSEnv.CLUSTER,
+        service=ECSEnv.SERVICE,
+        desiredCount=1)
+    wait_for_desired_count(ECSEnv.CLUSTER, ECSEnv.SERVICE, 1)
+
+
+def stop_streamer():
+    # To stop the streamer, set the desired count to 0 and wait
+    # until tasks are stopped
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ecs.html#ECS.Client.update_service
+    ecs.update_service(
+        cluster=ECSEnv.CLUSTER,
+        service=ECSEnv.SERVICE,
+        desiredCount=0)
+    wait_for_desired_count(ECSEnv.CLUSTER, ECSEnv.SERVICE, 0)
+
+
 def handle_stream_config():
     state = json.loads(get_s3_object(
         ECSEnv.BUCKET_NAME, ECSEnv.STREAM_STATE_S3_KEY))
+    state = state['state']
     response = s3.list_object_versions(
         Prefix=ECSEnv.STREAM_CONFIG_S3_KEY, Bucket=ECSEnv.BUCKET_NAME)
 
@@ -87,81 +110,40 @@ def handle_stream_config():
     config_manager_new = ConfigManager(version_id=version_ids[0])
 
     # Check if the streamer is currently active
-    streamer_currently_active = check_if_currently_active(
-        ECSEnv.CLUSTER, ECSEnv.SERVICE)
+    # streamer_currently_active = check_if_currently_active(
+    #     ECSEnv.CLUSTER, ECSEnv.SERVICE)
 
     # Restart streaming if configs are different
     if config_manager_old.write() != config_manager_new.write():
         if state is True:
-            logger.info('Config changed. Going to restart the streamer.')
-            # To stop streaming, set the desired count to 0 and wait
-            # until tasks are stopped
-            # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ecs.html#ECS.Client.update_service
-            response = ecs.update_service(
-                cluster=ECSEnv.CLUSTER,
-                service=ECSEnv.SERVICE,
-                desiredCount=0)
-            wait_for_desired_count(ECSEnv.CLUSTER, ECSEnv.SERVICE, 0)
-
-            # To restart, set the disired count and wait
-            # until a task is pending/running
-            response = ecs.update_service(
-                cluster=ECSEnv.CLUSTER,
-                service=ECSEnv.SERVICE,
-                desiredCount=1)
-            wait_for_desired_count(ECSEnv.CLUSTER, ECSEnv.SERVICE, 1)
-        elif state is False and streamer_currently_active:
-            logger.info('Config changed, the streamer is running '
-                        'but the current state is False. '
+            logger.info('The config changed. Going to restart the streamer.')
+            stop_streamer()
+            start_streamer()
+        elif state is False:
+            logger.info('The config changed, the current state is False. '
                         'Going to stop the streamer.')
-            # To stop streaming, set the desired count to 0 and wait
-            # until tasks are stopped
-            # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ecs.html#ECS.Client.update_service
-            response = ecs.update_service(
-                cluster=ECSEnv.CLUSTER,
-                service=ECSEnv.SERVICE,
-                desiredCount=0)
-            wait_for_desired_count(ECSEnv.CLUSTER, ECSEnv.SERVICE, 0)
+            stop_streamer()
 
 
 def handle_stream_state():
     state = json.loads(get_s3_object(
         ECSEnv.BUCKET_NAME, ECSEnv.STREAM_STATE_S3_KEY))
+    if state['from'] == 'rails':
+        return
+    state = state['state']
 
-    streamer_currently_active = check_if_currently_active(
-        ECSEnv.CLUSTER, ECSEnv.SERVICE)
+    # streamer_currently_active = check_if_currently_active(
+    #     ECSEnv.CLUSTER, ECSEnv.SERVICE)
 
     # - active => start streamer
     # - inactive => stop streamer
 
     if state is True:
         logger.info('The new state is True. Starting the streamer.')
-        if streamer_currently_active is False:
-            # To start streaming, set desired count to 1 and wait
-            # until tasks are pending
-            # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ecs.html#ECS.Client.update_service
-            _ = ecs.update_service(
-                cluster=ECSEnv.CLUSTER,
-                service=ECSEnv.SERVICE,
-                desiredCount=1)
-
-            wait_for_desired_count(
-                ECSEnv.CLUSTER, ECSEnv.SERVICE, 1)
-        elif streamer_currently_active is True:
-            logger.info('Streamer is already active.')
-
+        start_streamer()
     elif state is False:
         logger.info('The new state is False. Stopping the streamer.')
-
-        # To stop streaming, set desired count to 0 and wait
-        # until tasks are stopped
-        # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ecs.html#ECS.Client.update_service
-        _ = ecs.update_service(
-            cluster=ECSEnv.CLUSTER,
-            service=ECSEnv.SERVICE,
-            desiredCount=0)
-
-        wait_for_desired_count(ECSEnv.CLUSTER, ECSEnv.SERVICE, 0)
+        stop_streamer()
 
 
 def handler(event, context):
