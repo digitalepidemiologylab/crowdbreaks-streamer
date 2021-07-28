193ea9a1bfe8ee99491ff5dc9426e1aa7f6f4126
utanashati <utanashati@mail.ru>
2021-07-28 12:49:30 +0200

    No S3 event notif upon s3-to-es lambda creation

--- lambda-s3-to-es/run.py
+++ lambda-s3-to-es/run.py
@@ -31,8 +31,7 @@ def main():
         lambda_local_zip_path,
         policy_path,
         push_to_s3=True,
-        s3_trigger=True,
-        s3_prefix=AWSEnv.STORAGE_BUCKET_PREFIX,
+        s3_trigger=False,
         memory_size=512
     )
 
