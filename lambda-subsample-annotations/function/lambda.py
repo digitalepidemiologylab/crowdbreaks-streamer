3d08f76d072e1893c52b7acb2ed8667d1d60d252
Olesya Altunina <utanashati@mail.ru>
2022-03-14 08:27:54 +0100

    Filan fixes (reading and writing CSV to S3)

--- lambda-subsample-annotations/function/lambda.py
+++ lambda-subsample-annotations/function/lambda.py
@@ -5,13 +5,13 @@ import numpy as np
 import pandas as pd
 
 from awstools.session import s3
-from awstools.env import ECSEnv, AWSEnv
+from awstools.env import AWSEnv
 from awstools.s3 import get_s3_object
 
 FUNCTION_NAME = f'{AWSEnv.APP_NAME}-subsample-annotations-{AWSEnv.ENV}'
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 get_worker_tweets = lambda df, worker_id: df[df.worker_id == worker_id].tweet_id.unique()
 
@@ -46,6 +46,7 @@ def sample_tweets_from_each_worker(df, n_samples=1):
                 ]))
         else:
             unsampled_worker_ids = unique_workers
+        logger.debug('sample_tweets_from_each_worker: %s', unsampled_worker_ids)
         worker_id = np.random.choice(unsampled_worker_ids, 1)[0]
 
         df_worker = df[df.worker_id == worker_id]
@@ -65,6 +66,7 @@ def sample_more_tweets_for_min_workers(df, df_sampled, n_samples=1, min_tweet_co
     while min(get_tweet_counts(unique_workers, df_sampled).values()) < min_tweet_count:
         min_count = min(tweet_counts.values())
         workers_with_min_count = [k for k, v in tweet_counts.items() if v == min_count]
+        logger.debug('sample_more_tweets_for_min_workers: %s', workers_with_min_count)
         worker_id = np.random.choice(workers_with_min_count, 1)[0]
         worker_tweets = get_worker_tweets(df, worker_id)
 
@@ -74,7 +76,10 @@ def sample_more_tweets_for_min_workers(df, df_sampled, n_samples=1, min_tweet_co
                 tweet_id in df_sampled.tweet_id.tolist()
                 for tweet_id in worker_tweets.tolist()
             ]))
+        if len(unsampled_worker_tweets) == 0:
+            return df_sampled
 
+        logger.debug('sample_more_tweets_for_min_workers 2: %s', unsampled_worker_tweets)
         sampled_worker_tweets = np.random.choice(unsampled_worker_tweets, n_samples, replace=False)
         worker_tweets_in_df = [tweet_id in sampled_worker_tweets.tolist() for tweet_id in df.tweet_id]
         sampled_tweets = df[worker_tweets_in_df]
@@ -100,13 +105,20 @@ def handler(event, context):
     if 'results' not in key:
         logger.info("'%s' is not a results object.", key)
         return
-
+    # Avoid vicious cycle
+    elif key.endswith('_evaluate.csv'):
+        logger.warning("'%s' is a subsample already.", key)
+        return
     key_name = '.'.join(key.split('.')[:-1])
-    output_key = key_name + '_evaluate.csv'
 
-    df = pd.read_csv(get_s3_object(ECSEnv.BUCKET_NAME, key))
-    df_sampled = sample_tweets_for_all_workers(df, n_samples=5, min_tweet_count=10)
+    output_key = key_name + '_evaluate.csv'
+    logger.debug(output_key)
+
+    df = pd.read_csv(StringIO(get_s3_object(AWSEnv.BUCKET_NAME, key)))
+    logger.info('Unique tweets in the original sample: %s', len(df.tweet_id.unique()))
+    df_sampled = sample_tweets_for_all_workers(df, n_samples=1, min_tweet_count=10)
+    logger.info('Unique tweets in the subsample: %s', len(df_sampled.tweet_id.unique()))
 
     csv_buffer = StringIO()
     df_sampled.to_csv(csv_buffer)
-    s3.put_object(Body=csv_buffer.getvalue(), Key=output_key)
+    s3.put_object(Body=csv_buffer.getvalue(), Bucket=AWSEnv.BUCKET_NAME, Key=output_key)
